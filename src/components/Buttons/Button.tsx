f20c1344f5b278773f0f1881e4ab1db3a5ee9b22
Канаев Андрей <andykanaev@yandex.ru>
2024-05-23 14:04:54 +0300

    Added button_fullwidth modificator

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -8,12 +8,13 @@ export interface ButtonProps {
     onClick: () => void;
     icon?: ReactNode;
     size?: BUTTON_SIZE;
-    color?: BUTTON_COLOR
+    color?: BUTTON_COLOR;
+    fullwidth?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({text, onClick, icon, size, color}) => {
+const Button: FC<ButtonProps> = ({text, onClick, icon, size, color, fullwidth}) => {
     return (
-        <button className={`button button_color_${color ? color : 'default'} button_size_${size ? size : 'md'}`} onClick={onClick}>
+        <button className={`button button_color_${color ? color : 'default'} button_size_${size ? size : 'md'} ${fullwidth ? 'button_fullwidth' : null}`} onClick={onClick}>
             {icon ? <span className="button__icon">{icon}</span> : null}
             <span className="button__text">{text}</span>
         </button>
