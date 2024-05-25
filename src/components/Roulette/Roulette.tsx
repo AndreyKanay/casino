3b5c61950e91016a22ce3959dff79f4a3c346c9a
Канаев Андрей <andykanaev@yandex.ru>
2024-05-25 05:32:17 +0300

    Added Spinner Component

--- src/components/Roulette/Roulette.tsx
+++ src/components/Roulette/Roulette.tsx
@@ -2,13 +2,28 @@ import Bet from "../Bet/Bet";
 import RouletteState from "../../state/RouletteState";
 import {observer} from "mobx-react-lite";
 import Participants from "../Participants/Participants";
+import Spinner from "../Spinner/Spinner";
+
+import "./Roulette.css";
 
 const Roulette = () => {
     return (
         <div className="roulette">
-            <div className="roulette__text"></div>
+            <div className="roulette__text">
+                6,34 sec
+            </div>
             <div className="roulette__spiner">
-
+                <Spinner items={[
+                    {bet: 10, color: "#9A6BFF"},
+                    {bet: 20, color: "#EF0358"},
+                    {bet: 30, color: "#EF7403"},
+                    {bet: 5, color: "#EAEF03"},
+                    {bet: 10, color: "#0337EF"},
+                    {bet: 15, color: "#03C4EF"},
+                    {bet: 10, color: "#EA03EF"},
+                    {bet: 10, color: "#3332AA"},
+                    {bet: 1, color: "#c2ff32"},
+                ]} />
             </div>
             {RouletteState.isBet ?
                 null :
---
+++ src/components/SVG/SVGArc.tsx
@@ -0,0 +1,73 @@
+import {FC} from "react";
+import {polarToCartesian} from "../../helpers/polarToCartesian";
+import {getArcPath} from "../../helpers/getArcPath";
+
+interface SVGArcProps {
+    color: string;
+    startAngle: number;
+    endAngle: number;
+    radius: number;
+    cutoutRadius: number;
+}
+
+const SVGArc:FC<SVGArcProps> = ({color, startAngle, endAngle, radius, cutoutRadius}) => {
+    if (endAngle - startAngle >= 360) {
+        const largeArcFlag = 0
+        const outerStart = polarToCartesian(radius, radius, 0)
+        const outerMid = polarToCartesian(radius, radius, 180)
+        const outerEnd = polarToCartesian(radius, radius, 360)
+        const innerStart = polarToCartesian(radius, cutoutRadius, 0)
+        const innerMid = polarToCartesian(radius, cutoutRadius, 180)
+        const innerEnd = polarToCartesian(radius, cutoutRadius, 360)
+
+        const path = [
+            'M',
+            outerStart.x,
+            outerStart.y,
+            'A',
+            radius,
+            radius,
+            0,
+            largeArcFlag,
+            1,
+            outerMid.x,
+            outerMid.y,
+            'A',
+            radius,
+            radius,
+            0,
+            largeArcFlag,
+            1,
+            outerEnd.x,
+            outerEnd.y,
+            'L',
+            innerEnd.x,
+            innerEnd.y,
+            'A',
+            cutoutRadius,
+            cutoutRadius,
+            0,
+            largeArcFlag,
+            0,
+            innerMid.x,
+            innerMid.y,
+            'A',
+            cutoutRadius,
+            cutoutRadius,
+            0,
+            largeArcFlag,
+            0,
+            innerStart.x,
+            innerStart.y,
+            'Z',
+        ].join(' ')
+
+        return <path fill={color} d={path} />
+    }
+
+    const path = getArcPath(radius, cutoutRadius, startAngle, endAngle)
+
+    return <path fillRule="evenodd" fill={color} d={path} />
+}
+
+export default SVGArc;
