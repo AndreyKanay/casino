32b8ba53f4c8aab4932918b78da0ed03accfaf57
Канаев Андрей <andykanaev@yandex.ru>
2024-05-25 16:26:34 +0300

    Modal Markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import {RouterProvider} from "react-router";
 import router from "./router";
+import Modal from "./components/Modal/Modal";
 
 function App() {
-  return <RouterProvider router={router} />;
+    return (
+        <>
+            <RouterProvider router={router} />
+            <Modal title="faq" isShow={true} closeButtonText="Играть" onClose={() => {
+            }}>
+                <p>
+                    Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac
+                    aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                    inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur
+                    neque.Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit
+                    interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia
+                    nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo
+                    efficitur neque.Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et
+                    velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia
+                    nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo
+                    efficitur neque.Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et
+                    velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia
+                    nostra, per inceptos himenaeos.
+                </p>
+            </Modal>
+        </>
+    );
 }
 
 export default App;
